48e9ce31e34ff0ec0128ae81cf0862611b9cef8e
GhosterZz <alfiejennings2021@gmail.com>
2021-11-02 21:59:19 +0000

    added a discord redirect

--- server.js
+++ server.js
@@ -1,13 +1,9 @@
 const express = require('express')
 const app = express()
- const path = require('path')
 
 app.use(express.static(__dirname + '/public'));
 
-
- app.get("/discord", (req, res) => {
-res.status(301).redirect("https://discord.gg/gTmh5TcEwR")
-})
+app.use("/discord", (express.static('./public/discord.html')))
  app.use("/home", (express.static('./public/home.html')))
  app.use("/about", (express.static('./public/about.html')))
 app.listen()
