b5fe20b1f95d5851db8d22f48bb2cd491625b61b
GhosterZz <alfiejennings2021@gmail.com>
2021-11-02 21:18:38 +0000

    UPDATED WITH PAGES

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const express = require('express')
 const app = express()
- 
+ const path = require('path')
 
 app.use(express.static(__dirname + '/public'));
 
 
  app.get("/discord", (req, res) => {
-
-    res.status(301).redirect("https://discord.gg/kPBufSFmuJ")
-
+res.status(301).redirect("https://discord.gg/gTmh5TcEwR")
 })
+ app.use("/home", (express.static('./public/home.html')))
+ app.use("/about", (express.static('./public/about.html')))
 app.listen()
-console.log('Server running https://ghostcodes.herokuapp.com/')
+console.log('Server running https://ghostcodes.xyz')
 
 module.exports = app;
