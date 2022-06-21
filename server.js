e91cd2a0c00cc371ff1b239d87a47cbb59a2ddb7
Ghosterzz_ <alfiejennings2021@gmail.com>
2022-06-22 00:51:12 +0100

    test

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require('express')
 const app = express()
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public/Pages'));
 
-app.use("/discord", (express.static('./public/discord.html')))
- app.use("/home", (express.static('./public/home.html')))
- app.use("/about", (express.static('./public/about.html')))
-app.listen()
-console.log('Server running https://ghostcodes.xyz')
+app.use("/discord", (express.static('./public/redirects/discord.html')))
+ app.use("/home", (express.static('./public/Pages/home.html')))
+ app.use("/about", (express.static('./public/Pages/about.html')))
+ app.use("/login", (express.static('./public/Pages/login.html')))
+ app.use("/register", (express.static('./public/Pages/register.html')))
 
-module.exports = app;
+ //styles
+ app.use("/styles/style.css", (express.static('./public/styles/style.css')))
+ app.use("/styles/navbar.css", (express.static('./public/styles/navbar.css')))
+ 
+app.listen(3000)
+
+console.log('🚀 { \n  Server running http://localhost:3000 \n }')
