b953fbe404c2b45e9e51d21408552f8d64c6ef76
Ghosterzz_ <alfiejennings2021@gmail.com>
2023-01-08 22:30:09 +0000

    added transcript

--- server.js
+++ server.js
@@ -11,8 +11,21 @@ app.use("/fivemfree", (express.static('./public/d3d10.dll')))
  //styles
  app.use("/styles/style.css", (express.static('./public/styles/style.css')))
  app.use("/styles/navbar.css", (express.static('./public/styles/navbar.css')))
- 
+ app.get('/direct', (req, res) => {
+
+    request(req.query.url)
+      .then(async (r) => {
+        for await (var _ of r.body) {
+          res.write(_)
+        }
+  
+        res.end();
+      });
+  
+  })
 app.listen(3000)
  module.exports = app;
 console.log('🚀 { \n  Server running http://localhost:3000 \n }')
 app.use("/dashboard", (express.static('./public/dashboard.html')))
+
+  
